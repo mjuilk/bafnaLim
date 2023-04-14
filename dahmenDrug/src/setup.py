24b0ad8614c1bf90c33ea163e961dfac6e6906a2
mjuilk <jonconneryl@gmail.com>
2023-04-14 16:13:33 +0100

    .bat works, .exe doesn't

---
+++ dahmenDrug/src/setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 14 15:55:27 2023
+
+@author: jon
+"""
+
+from distutils.core import setup
+import py2exe
+
+setup(console = ['gui.py'], py_modules = [])
